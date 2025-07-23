bd057fde932cf2354afbb8c2845112c481edaece
maryam jafari <maryamjafari12175@gmail.com>
2025-07-23 22:09:37 +0330

    -update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 import random
 
 app = Flask(__name__)
@@ -6,25 +6,31 @@ app.secret_key = "super-secret-key"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if "number" not in session:
-        session["number"] = random.randint(1, 50)
-        session["count"] = 0
-
-
-    message = ""
     if request.method == "POST":
         guess = int(request.form["guess"])
+
+        if "number" not in session:
+            session["number"] = random.randint(1, 50)
+            session["count"] = 0
+
         session["count"] += 1
 
         if guess < session["number"]:
-            message = "❌ Nope! My number is higher! 🔼"
+            session["message"] = "❌ Nope! My number is higher! 🔼"
         elif guess > session["number"]:
-            message = "❌ Nah! My number is lower! 🔽"
+            session["message"] = "❌ Nah! My number is lower! 🔽"
         else:
-            message = f"🎉 Whoa! You got it in {session['count']} tries! 🏆"
+            session["message"] = f"🎉 Whoa! You got it in {session['count']} tries! 🏆"
             session.pop("number")
             session.pop("count")
-    return render_template("index.html", message=message)
+
+        return redirect(url_for("index"))
+
+    # حالت GET
+    message = session.pop("message", "")
+    show_game = "number" in session and message != ""
+
+    return render_template("index.html", message=message, show_game=show_game)
 
 if __name__ == "__main__":
     app.run(debug=True)
